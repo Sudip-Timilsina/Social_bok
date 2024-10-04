39f9b2f48ca29f4814c631fda41341cdc323698d
Sudip-Timilsina <timilsinasudip980@gmail.com>
2024-10-04 10:05:33 +0545

    adding pipfile

---
+++ social_book/core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("",views.index,name='index'),
+    path("signup",views.signup,name='signup'),
+    path("signin",views.signin,name='signin'),
+    path("logout",views.logout,name='logout'),
+    path("setting",views.settings,name='setting'),
+    path("upload",views.upload,name='upload'),
+    path("like-post",views.like_post,name='like-post'),
+    path("profile/<str:pk>",views.profile,name='profile'),
+    path("follow",views.follow,name='follow'),
+    path("search",views.search,name='search'),
+
+
+
+
+
+]
